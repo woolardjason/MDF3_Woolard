46c993c2c91cdf3c57523dcc5e0cb5e518ec2c45
Jason Woolard <woolardjason@gmail.com>
2014-02-06 21:51:27 -0500

    Added in second app to be used to call intent

---
+++ Week 1/Planit2/src/com/jasonwoolard/planit2/MainActivity.java
@@ -0,0 +1,48 @@
+package com.jasonwoolard.planit2;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		Button searchBtn = (Button) findViewById(R.id.button1);
+		
+		searchBtn.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+			
+				EditText latitude = (EditText) findViewById(R.id.editText1);
+				EditText longitude = (EditText) findViewById(R.id.editText2);
+				String myUri =  "https://maps.google.com/maps?q=" + latitude.getText().toString() + ","+ longitude.getText().toString();
+				Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(myUri));
+
+				intent.putExtra("longitude", longitude.getText().toString());
+				intent.putExtra("latitude", latitude.getText().toString());
+
+			    String title = (String) getResources().getText(R.string.menu_title);
+		        Intent chooser = Intent.createChooser(intent, title);
+		        startActivityForResult(chooser, 1);
+				
+			}
+		});
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
